96e976a082e26664ee0d316b425f59516b12562d
Pedro Alves <pedro.m.raposoalves@gmail.com>
2013-12-10 00:33:56 +0000

    Imagens

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
 	belongs_to :category
 	validates :titulo, :presence => true, :length => {:minimum => 10}
 	validates :conteudo, :presence => true
+	has_attached_file :avatar, :styles => { :medium => "600x600>", :small => "300x300>" }, :default_url => "/images/:style/missing.png"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131126193634) do
+ActiveRecord::Schema.define(version: 20131209201353) do
 
   create_table "comentarios", force: true do |t|
     t.string   "autor"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20131126193634) do
     t.text     "conteudo"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
