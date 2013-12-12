3a10ee5fdce600b49328fa2c02ed8aaea53d4279
Pedro Alves <pedro.m.raposoalves@gmail.com>
2013-12-12 21:20:52 +0000

    Layout com BootStrap e imagens

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ActiveRecord::Base
 	has_many :comentarios
-	belongs_to :category
 	validates :titulo, :presence => true, :length => {:minimum => 10}
 	validates :conteudo, :presence => true
-	has_attached_file :avatar, :styles => { :medium => "600x600>", :small => "300x300>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "600x600>", :small => "400x400>" }, :default_url => "/images/:style/missing.png"
 end
